93960803c365e08d04115341319db922162507cb
snimo <sebastian@zenithtek.io>
2025-08-09 21:29:28 -0300

    Tool time-zone added

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ with open("prompts.yaml", 'r') as stream:
 
 agent = CodeAgent(
     model=model,
-    tools=[final_answer],  ## add your tools here (don't remove final answer)
+    tools=[final_answer , get_current_time_in_timezone],  ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
